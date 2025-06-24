c942c5860fbbec94140bb57088c47870285914f4
m2data <48026012+m2data@users.noreply.github.com>
2025-06-24 12:17:03 +0200

    addded DDL for the new tables

---
+++ Kundensystem/5/_Kunden_DDL_5.sql
@@ -0,0 +1,15 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE [Kunden_abc_20250102_15012025] (
+    [CustomerID] varchar(30)  NOT NULL ,
+    [ExternalID] char(13)  NOT NULL ,
+    [CustomerStatus] varchar(30)  NULL ,
+    [CustomerDiscount] numeric(5,2)  NULL ,
+    [ValidFrom] date  NOT NULL ,
+    CONSTRAINT [PK_Kunden] PRIMARY KEY CLUSTERED (
+        [CustomerID] ASC
+    )
+)
+
---
+++ Produktkatalog/6/_Produktkategorie_DDL_6.sql
@@ -0,0 +1,17 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE [Produktkategorie_20250103] (
+    [KatID] varchar(50)  NOT NULL ,
+    [OberKatID] varchar(50)  NULL ,
+    [Name] varchar(512)  NOT NULL ,
+    [Rabatt] numeric(5,2)  NULL ,
+    [Von] date  NOT NULL ,
+    [Bis] date  NULL ,
+    [Festlegung] date  NOT NULL ,
+    CONSTRAINT [PK_Kategorie] PRIMARY KEY CLUSTERED (
+        [KatID] ASC
+    )
+)
+
